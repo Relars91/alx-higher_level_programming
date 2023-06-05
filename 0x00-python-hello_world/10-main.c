a4f8351edd575d7a8e5c22119cfcb33d9e174128
Uchechukwu Madumere <madumereuchechukwu@gmail.com>
2023-06-05 13:55:18 -0700

    commit10

--- 0x00-python-hello_world/10-main.c
+++ 0x00-python-hello_world/10-main.c
@@ -29,4 +29,25 @@ int main(void)
 	if (check_cycle(head) == 0)
 		printf("Linked list has no cycle\n");
 	else if (check_cycle(head) == 1)
+		printf("Linked list has a cycle\n");
 
+	current = head;
+	for (i = 0; i < 4; i++)
+		current = current->next;
+	temp = current->next;
+	current->next = head;
+
+	if (check_cycle(head) == 0)
+		printf("Linked list has no cycle\n");
+	else if (check_cycle(head) == 1)
+		printf("Linked list has a cycle\n");
+
+	current = head;
+	for (i = 0; i < 4; i++)
+		current = current->next;
+	current->next = temp;
+
+	free_listint(head);
+
+	return (0);
+}

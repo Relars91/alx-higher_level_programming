9f683b98623ab5eee1039676a82fdb39d58dfc5c
Uchechukwu Madumere <madumereuchechukwu@gmail.com>
2023-06-12 14:01:28 -0700

    second

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 and idx > (len(my_list) - 1):
-        return None
+    if idx <  0:
+        return
+    if idx >= len(my_list):
+        return
     else:
         return (my_list[idx])

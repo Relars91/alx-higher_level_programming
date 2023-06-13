ea38a1c51f8264e1a026410f1694cdd1bba8191f
Uchechukwu Madumere <madumereuchechukwu@gmail.com>
2023-06-13 16:53:36 -0700

    corrections

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+
 def infinite(args):
-    sum = 0
+    total = 0
     for a in args[1:]:
-        sum += int(a)
-    print(sum)
+        total += int(a)
+    print(total)
+
 
 if __name__ == "__main__":
     import sys
+
     infinite(sys.argv)
